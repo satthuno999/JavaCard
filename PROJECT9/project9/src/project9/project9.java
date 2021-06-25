d39a3cc35aeaf9d3c64cfc9377c315c6ebff9b80
satthuno999 <41618648+satthuno999@users.noreply.github.com>
2021-06-25 15:11:13 +0700

    input infor AES - RSA

--- PROJECT9/project9/src/project9/project9.java
+++ PROJECT9/project9/src/project9/project9.java
@@ -6,6 +6,12 @@ import javacard.security.KeyBuilder;
 import javacard.security.RandomData;
 import javacardx.crypto.Cipher;
 import javacard.security.AESKey;
+import javacard.security.Key;
+import javacard.security.KeyAgreement;
+import javacard.security.KeyPair;
+import javacard.security.RSAPrivateCrtKey;
+import javacard.security.RSAPrivateKey;
+import javacard.security.RSAPublicKey;
 
 public class project9 extends Applet
 {
@@ -18,6 +24,33 @@ public class project9 extends Applet
 	private final static byte PIN_MIN_SIZE = (byte) 4;
 	private final static byte PIN_MAX_SIZE = (byte) 16;
 	private final static byte[] PIN_INIT_VALUE={(byte)'S',(byte)'p',(byte)'a',(byte)'r',(byte)'k',(byte)'9',(byte)'9'};
+	//Information
+	public static byte[] OpData = new byte[6];
+	public static byte lenData = (byte)0;
+	
+	public static byte[] OpID = new byte[6];
+	public static byte lenID = (byte)0;
+	public static byte[] OpNAME = new byte[25];
+	public static byte lenNAME = (byte)0;
+	public static byte[] OpDATE = new byte[10];
+	public static byte lenDATE= (byte)0;
+	public static byte[] OpPHONE = new byte[10];
+	public static byte lenPHONE= (byte)0;
+	
+	public static byte[] OpImage,size;
+	
+	
+	// Maximum number of keys handled by the Cardlet
+	private final static byte MAX_NUM_KEYS = (byte) 16;
+	//object ID
+	private final static byte KEY_ACL_SIZE = (byte) 6;
+	private final static byte ACL_READ = (byte) 0;
+	private final static byte ACL_WRITE = (byte) 2;
+	private final static byte ACL_USE = (byte) 4;
+	// INS - Khi to
+	private final static byte INS_SETUP = (byte) 0x2A;
+	// Keys' use and management
+	private final static byte INS_GEN_KEYPAIR = (byte) 0x30;
 	//INS-PIN
 	private final static byte INS_CREATE_PIN = (byte) 0x40;
 	private final static byte INS_VERIFY_PIN = (byte) 0x42;
@@ -25,21 +58,61 @@ public class project9 extends Applet
 	private final static byte INS_UNBLOCK_PIN = (byte) 0x46;
 	
 	private final static byte INS_CREATE_INFORMATION = (byte)0x50;
+	private final static byte INS_OUT_INFORMATION = (byte)0x51;
+	private final static byte OUT_ID = (byte)0x01;
+	private final static byte OUT_NAME = (byte)0x02;
+	private final static byte OUT_DATE = (byte)0x03;
+	private final static byte OUT_PHONE = (byte)0x04;
+	
+	private final static byte INS_CREATE_IMAGE = (byte)0x52;
+	private final static byte INS_CREATE_SIZEIMAGE = (byte)0x54;
+	private final static byte INS_OUT_SIZEIMAGE = (byte)0x56;
+	private final static byte INS_OUT_IMAGE = (byte)0x58;
+	
 	private final static byte INS_LOGOUT_ALL = (byte) 0x60;
+	// Cipher Modes admitted in ComputeCrypt
+	private final static byte OPT_DEFAULT = (byte) 0x00; // Use JC defaults
+	private final static byte OPT_RSA_PUB_EXP = (byte) 0x01; // RSA: provide public exponent
+	// Standard public ACL
+	private static byte[] STD_PUBLIC_ACL;
+	private static byte[] acl; // Temporary ACL
 	/* Kim tra trng thi setup */
 	private boolean setupDone = false;
-	// INS - Khi to
-	private final static byte INS_SETUP = (byte) 0x2A;
+	private byte create_object_ACL;
+	private byte create_key_ACL;
+	private byte create_pin_ACL;
+	
+
 	//PIN - m s nhn dng c nhn lu trong pins
 	//PUK - m m kho c nhn lu trong unlk_pins
 	private OwnerPIN pin, ublk_pin;
 	/** ghi trng thi ng nhp*/
 	private short logged_ids;
 	
+	// Key objects (allocated on demand)
+	private Key[] keys;
+	// Key ACLs
+	private byte[] keyACLs;
+	// Key Tries Left
+	private byte[] keyTries;
+	// Key iterator for ListKeys: it's an offset in the keys[] array.
+	private byte key_it;
+	//crypt
 	private Cipher aesCipher;
 	private AESKey aesKey;
 	private static short KEY_SIZE= 32;
-	
+	/*****
+	*RSA**
+	*****/
+	private KeyPair[] keyPairs;
+	// Offsets in buffer[] for key generation
+	private final static short OFFSET_GENKEY_ALG = (short) (ISO7816.OFFSET_CDATA);
+	private final static short OFFSET_GENKEY_SIZE = (short) (ISO7816.OFFSET_CDATA + 1);
+	private final static short OFFSET_GENKEY_PRV_ACL = (short) (ISO7816.OFFSET_CDATA + 3);
+	private final static short OFFSET_GENKEY_PUB_ACL = (short) (OFFSET_GENKEY_PRV_ACL + KEY_ACL_SIZE);
+	private final static short OFFSET_GENKEY_OPTIONS = (short) (OFFSET_GENKEY_PUB_ACL + KEY_ACL_SIZE);
+	private final static short OFFSET_GENKEY_RSA_PUB_EXP_LENGTH = (short) (OFFSET_GENKEY_OPTIONS + 1);
+	private final static short OFFSET_GENKEY_RSA_PUB_EXP_VALUE = (short) (OFFSET_GENKEY_RSA_PUB_EXP_LENGTH + 2);
 	/**
 	* ERROR CONTROL*
 	**/
@@ -59,9 +132,16 @@ public class project9 extends Applet
 	private final static short SW_INCORRECT_P1 = (short) 0x9C10;
 	/** Li P2*/
 	private final static short SW_INCORRECT_P2 = (short) 0x9C11;
+	/** Required operation was not authorized because of a lack of privileges */
+	private final static short SW_UNAUTHORIZED = (short) 0x9C06;
+	/** Algorithm specified is not correct */
+	private final static short SW_INCORRECT_ALG = (short) 0x9C09;
+	
 	private project9(byte[] bArray, short bOffset, byte bLength){
 		if (!CheckPINPolicy(PIN_INIT_VALUE, (short) 0, (byte) PIN_INIT_VALUE.length))
 		    ISOException.throwIt(SW_INTERNAL_ERROR);
+		OpImage = new byte[10000];
+		size = new byte[7];
 
 		/* Ci gi tr pin khi to*/
 		pin = new OwnerPIN((byte) 3, (byte) PIN_INIT_VALUE.length);
@@ -123,6 +203,24 @@ public class project9 extends Applet
 			break;
 		case INS_CREATE_INFORMATION:
 			SetupInformation(apdu,buffer);
+			break;
+		case INS_OUT_INFORMATION:
+			OutputInformation(apdu,buffer);
+			break;
+		case INS_CREATE_IMAGE:
+			SetupImage(apdu,buffer);
+			break;
+		case INS_CREATE_SIZEIMAGE:
+			SetCount(apdu,buffer);
+			break;
+		case INS_OUT_SIZEIMAGE:
+			OuputSize(apdu,buffer);
+			break;
+		case INS_OUT_IMAGE:
+			OututImage(apdu,buffer);
+			break;
+		case INS_GEN_KEYPAIR:
+			GenerateKeyPair(apdu, buffer);
 			break;
 		default:
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
@@ -282,7 +380,115 @@ public class project9 extends Applet
 		pin.resetAndUnblock();
 	}
 	private void SetupInformation(APDU apdu, byte[] buffer){
-		
+			lenData = buffer[ISO7816.OFFSET_LC];
+			if(lenData>(byte) 32){
+				ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+			}
+			
+			Util.arrayCopyNonAtomic(buffer,ISO7816.OFFSET_CDATA,OpData,(short) 0x00,lenData);
+					
+			short flag = (short)0;
+			byte[] objData = new byte[6];
+			byte objDatai = 0;
+			for(short i=0;i<=lenData;i++){
+				if((byte)(OpData[i]) == (byte)0x02){
+					flag = (short)1;
+					continue;
+				}
+				else if((byte)(OpData[i]) == (byte)0x03){
+					flag = (short)0;
+				};
+					
+				if(flag == (short)1){
+					objData[objDatai] = OpData[i];
+					objDatai++;
+				}
+				else if(flag == (short)0 && objDatai != (byte)0){
+					if(lenID == (byte)0){
+						if(objDatai > 6){
+							ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+						}
+						byte[] temp = encrypt(objData);
+						Util.arrayCopyNonAtomic(temp,(short)0x00,OpID,(short)0x00,(byte)(temp.length));
+						lenID = (byte)(objDatai);
+						objData = new byte[25];
+					}
+					else if(lenNAME == (byte)0){
+						if(objDatai > 25){
+							ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+						}
+						byte[] temp = encrypt(objData);
+						Util.arrayCopyNonAtomic(temp,(short)0x00,OpNAME,(short)0x00,(byte)(temp.length));
+						lenNAME = (byte)(objDatai);
+						objData = new byte[10];
+					}
+					else if(lenDATE == (byte)0){
+						if(objDatai > 10){
+							ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+						}
+						byte[] temp = encrypt(objData);
+						Util.arrayCopyNonAtomic(temp,(short)0x00,OpDATE,(short)0x00,(byte)(temp.length));
+						lenDATE = (byte)(objDatai);
+						objData = new byte[10];
+					}
+					else{
+						if(objDatai > 10){
+							ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+						}
+						byte[] temp = encrypt(objData);
+						Util.arrayCopyNonAtomic(temp,(short)0x00,OpPHONE,(short)0x00,(byte)(temp.length));
+						lenPHONE = (byte)(objDatai);
+					}
+					objDatai = (short)0;
+				};
+			}
+	}
+	private void OutputInformation(APDU apdu, byte[] buffer){
+			if(buffer[ISO7816.OFFSET_P1] == OUT_ID){
+				apdu.setOutgoing();
+				apdu.setOutgoingLength(lenID);
+				Util.arrayCopy(decrypt(OpID),(short)0,buffer,(short)0,lenID);
+				apdu.sendBytes((short)0,lenID);
+			}
+			else if(buffer[ISO7816.OFFSET_P1] == OUT_NAME){
+				apdu.setOutgoing();
+				apdu.setOutgoingLength(lenNAME);
+				Util.arrayCopy(decrypt(OpNAME),(short)0,buffer,(short)0,lenNAME);
+				apdu.sendBytes((short)0,lenNAME);
+			}
+			else if(buffer[ISO7816.OFFSET_P1] == OUT_DATE){
+				apdu.setOutgoing();
+				apdu.setOutgoingLength(lenDATE);
+				Util.arrayCopy(decrypt(OpDATE),(short)0,buffer,(short)0,lenDATE);
+				apdu.sendBytes((short)0,lenDATE);
+			}
+			else if(buffer[ISO7816.OFFSET_P1] == OUT_PHONE){
+				apdu.setOutgoing();
+				apdu.setOutgoingLength(lenPHONE);
+				Util.arrayCopy(decrypt(OpPHONE),(short)0,buffer,(short)0,lenPHONE);
+				apdu.sendBytes((short)0,lenPHONE);
+			}
+	}
+	
+	//Input Image
+	private void SetupImage(APDU apdu, byte[] buffer){
+		short p1 = (short)(buffer[ISO7816.OFFSET_P1]&0xff);
+		short count = (short)(249 * p1);
+		Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, OpImage, count, (short)249);
+	}
+	private void SetCount(APDU apdu, byte[] buffer){
+		Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, size, (short)0, (short)7);
+	}
+	private void OuputSize(APDU apdu, byte[] buffer){
+		Util.arrayCopy(size, (short)0, buffer, (short)0, (short)(size.length));
+		apdu.setOutgoingAndSend((short)0,(short)7);
+	}
+	private void OututImage(APDU apdu, byte[] buffer){
+		apdu.setOutgoing();
+		short p = (short)(buffer[ISO7816.OFFSET_P1]&0xff);
+		short count = (short)(249 * p);
+		apdu.setOutgoingLength((short)249);
+		apdu.sendBytesLong(OpImage, count, (short)249);
 	}
 	/*CHECK PIN POLICY*/
 	private boolean CheckPINPolicy(byte[] pin_buffer, short pin_offset, byte pin_size) {
@@ -296,6 +502,7 @@ public class project9 extends Applet
 	}
 	
 	/*AES*/
+    /**Encrypt*/
 	private byte[] encrypt(byte[] encryptData) {
         aesCipher.init(aesKey, Cipher.MODE_ENCRYPT);
         short newLength = addPadding(encryptData, (short) 0, (short) encryptData.length);
@@ -304,7 +511,7 @@ public class project9 extends Applet
         return temp;
     }
 
-    /**
+    /**Decrypt
      * Decrypt data.
      */
     private byte[] decrypt(byte[] encryptData) {
@@ -315,7 +522,7 @@ public class project9 extends Applet
         
         return temp;
     }
-	 /**
+	 /**Add padding
      * Add padding for AES encryption.
      *
      * @param data
@@ -331,7 +538,7 @@ public class project9 extends Applet
         return length;
     }
 
-    /**
+    /**remove padding
      * remove padding from decrypted result.
      *
      * @param buffer
@@ -348,4 +555,109 @@ public class project9 extends Applet
         length--;
         return length;
     }
+    /************RSA
+    *     RSA   *
+    *************/
+    private void GenerateKeyPair(APDU apdu, byte[] buffer) {
+		short bytesLeft = Util.makeShort((byte) 0x00, buffer[ISO7816.OFFSET_LC]);
+		if (bytesLeft != apdu.setIncomingAndReceive())
+			ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+		byte alg_id = buffer[OFFSET_GENKEY_ALG];
+		switch (alg_id) {
+			case KeyPair.ALG_RSA:
+			case KeyPair.ALG_RSA_CRT:
+				GenerateKeyPairRSA(buffer);
+				break;
+			case KeyPair.ALG_EC_FP:
+				//GenerateKeyPairECFP(buffer);
+				break;
+			default:
+				ISOException.throwIt(SW_INCORRECT_ALG);
+		}
+	}
+    // Data has already been received 
+	private void GenerateKeyPairRSA(byte[] buffer) {
+		byte prv_key_nb = buffer[ISO7816.OFFSET_P1];
+		if ((prv_key_nb < 0) || (prv_key_nb >= MAX_NUM_KEYS))
+			ISOException.throwIt(SW_INCORRECT_P1);
+		byte pub_key_nb = buffer[ISO7816.OFFSET_P2];
+		if ((pub_key_nb < 0) || (pub_key_nb >= MAX_NUM_KEYS))
+			ISOException.throwIt(SW_INCORRECT_P2);
+		if (pub_key_nb == prv_key_nb)
+			ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+		byte alg_id = buffer[OFFSET_GENKEY_ALG];
+		short key_size = Util.getShort(buffer, OFFSET_GENKEY_SIZE);
+		byte options = buffer[OFFSET_GENKEY_OPTIONS];
+		RSAPublicKey pub_key = (RSAPublicKey) getKey(pub_key_nb, KeyBuilder.TYPE_RSA_PUBLIC, key_size);
+		RSAPrivateKey prv_key = (RSAPrivateKey) getKey(prv_key_nb, alg_id == KeyPair.ALG_RSA ? KeyBuilder.TYPE_RSA_PRIVATE : KeyBuilder.TYPE_RSA_CRT_PRIVATE,	key_size);
+		/* If we're going to overwrite a keyPair's contents, check ACL */
+		if (pub_key.isInitialized() && !authorizeKeyOp(pub_key_nb,ACL_WRITE))
+			ISOException.throwIt(SW_UNAUTHORIZED);
+		if (prv_key.isInitialized() && !authorizeKeyOp(prv_key_nb,ACL_WRITE))
+			ISOException.throwIt(SW_UNAUTHORIZED);
+		/* Store private key ACL */
+		Util.arrayCopy(buffer, OFFSET_GENKEY_PRV_ACL, keyACLs, (short) (prv_key_nb * KEY_ACL_SIZE), KEY_ACL_SIZE);
+		/* Store public key ACL */
+		Util.arrayCopy(buffer, OFFSET_GENKEY_PUB_ACL, keyACLs, (short) (pub_key_nb * KEY_ACL_SIZE), KEY_ACL_SIZE);
+		switch (options) {
+		case OPT_DEFAULT:
+			if (pub_key.isInitialized())
+				pub_key.clearKey();
+			break;
+		case OPT_RSA_PUB_EXP:
+			short exp_length = Util.getShort(buffer, OFFSET_GENKEY_RSA_PUB_EXP_LENGTH);
+			pub_key.setExponent(buffer, OFFSET_GENKEY_RSA_PUB_EXP_VALUE, exp_length);
+			break;
+		default:
+			ISOException.throwIt(SW_INVALID_PARAMETER);
+		}
+		if ((keyPairs[pub_key_nb] == null) && (keyPairs[prv_key_nb] == null)) {
+			keyPairs[pub_key_nb] = new KeyPair(pub_key, prv_key);
+			keyPairs[prv_key_nb] = keyPairs[pub_key_nb];
+		} else if (keyPairs[pub_key_nb] != keyPairs[prv_key_nb])
+			ISOException.throwIt(SW_OPERATION_NOT_ALLOWED);
+		KeyPair kp = keyPairs[pub_key_nb];
+		if ((kp.getPublic() != pub_key) || (kp.getPrivate() != prv_key))
+			// This should never happen according with this Applet policies
+			ISOException.throwIt(SW_INTERNAL_ERROR);
+		// We Rely on genKeyPair() to make all necessary checks about types
+		kp.genKeyPair();
+	}
+	
+	//get Key
+	private Key getKey(byte key_nb, byte key_type, short key_size) {
+		
+		if (keys[key_nb] == null) {
+			// We have to create the Key
+
+			/* Check that Identity n.0 is logged */
+			if ((create_key_ACL == (byte) 0xFF)
+					|| (((logged_ids & create_key_ACL) == (short) 0x0000) && (create_key_ACL != (byte) 0x00)))
+				ISOException.throwIt(SW_UNAUTHORIZED);
+			
+			keys[key_nb] = KeyBuilder.buildKey(key_type, key_size, false);
+			
+		} else {
+			// Key already exists: check size & type
+			/*
+			 * TODO: As an option, we could just discard and recreate if not of
+			 * the correct type, but creates trash objects
+			 */
+			if ((keys[key_nb].getSize() != key_size) || (keys[key_nb].getType() != key_type))
+				ISOException.throwIt(SW_OPERATION_NOT_ALLOWED);
+		}
+		return keys[key_nb];
+	}
+	/** Check from key_nb key ACL if an operation can be done */
+	boolean authorizeKeyOp(byte key_nb, byte op) {
+		short acl_offset = (short) (key_nb * KEY_ACL_SIZE+ op);
+		short required_ids = Util.getShort(keyACLs, acl_offset);
+		return ((required_ids != (short) 0xFFFF) && ((short) (required_ids & logged_ids) == required_ids));
+	}
+	
+	/** Check from ACL if the corresponding key can be overwritten */
+	boolean authorizeKeyOp(byte[] ACL, byte op) {
+		short required_ids = Util.getShort(ACL, (short)op);
+		return ((required_ids != (short) 0xFFFF) && ((short) (required_ids & logged_ids) == required_ids));
+	}
 }
