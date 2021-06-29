0316655dd627a65f137c9a65d60c1525efa3d988
satthuno999 <41618648+satthuno999@users.noreply.github.com>
2021-06-29 19:52:07 +0700

    connect

--- GUI/JavaCard/JavaCard/src/javacard/ConnectCard.java
+++ GUI/JavaCard/JavaCard/src/javacard/ConnectCard.java
@@ -5,8 +5,13 @@
  */
 package javacard;
 
+import javax.smartcardio.CardTerminal;
 import javax.smartcardio.TerminalFactory;
 import jdk.nashorn.internal.ir.Terminal;
+import java.util.List;
+import javax.smartcardio.*;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 
 /**
  *
@@ -16,6 +21,16 @@ public class ConnectCard {
     public void connectapplet(){
         try{
             TerminalFactory factory = TerminalFactory.getDefault();
+            List<CardTerminal> terminals = factory.terminals().list();
+            
+            CardTerminal terminal = terminals.get(0);
+            
+            Card card = terminal.connect("*");
+            
+            CardChannel channel = card.getBasicChannel();
+            
+            byte[] aid = {(byte)0x25,(byte)0x10,(byte)0x19,(byte)0x99,(byte)0x00,(byte)0x00,(byte)0x00};
+            ResponseAPDU answer = channel.transmit(new CommandAPDU(0x00,0xA4,0x04,0x00,aid));
         }
         catch(Exception ex){
         }
